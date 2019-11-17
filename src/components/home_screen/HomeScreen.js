7b1ddac3000180c7b1cbbf016dd6e4ca3816d8fd
David Szenczewski <david@Davids-Air.fios-router.home>
2019-11-17 17:44:00 -0500

    Add new list

--- src/components/home_screen/HomeScreen.js
+++ src/components/home_screen/HomeScreen.js
@@ -4,8 +4,29 @@ import { compose } from 'redux';
 import { NavLink, Redirect } from 'react-router-dom';
 import { firestoreConnect } from 'react-redux-firebase';
 import TodoListLinks from './TodoListLinks'
+import { getFirestore } from 'redux-firestore';
 
+//const db = getFirestore();
+let key = -1;
 class HomeScreen extends Component {
+    
+    handleNewList = () => {
+        console.log("HURRAY");
+        let item = {}
+        item.description = "unknown";
+        item.assigned_to = "unknown";
+        item.due_date = "unknown";
+        item.completed = "false";
+        item.key = 0;
+        key = key + 1;
+        getFirestore().collection('todoLists').doc(key.toString()).set({
+            name: "Unknown",
+            owner: "Unkown",
+            key: key,
+            items: [item],
+        });
+        this.render();
+    }    
 
     render() {
         if (!this.props.auth.uid) {
@@ -36,7 +57,25 @@ class HomeScreen extends Component {
         );
     }
 }
-
+const createTodoList = todoList => (dispatch, getState, { getFirestore }) => {
+    const fireStore = getFirestore();
+    const { profile } = getState().firebase;
+    const authorId = getState().firebase.auth.uid;
+    fireStore.collection('todoLists').add({
+      ...todoList,
+      authorFirstName: profile.firstName,
+      authorLastName: profile.lastName,
+      authorId,
+      createdAt: new Date(),
+    }).then(() => dispatch({
+      type: 'CREATE_TODO_LIST',
+      todoList,
+    })).catch(err => dispatch({
+      type: 'CREATE_TODO_LIST_ERROR',
+      err,
+    }));
+  };
+  
 const mapStateToProps = (state) => {
     return {
         auth: state.firebase.auth
