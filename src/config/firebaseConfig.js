33d4beea08d77ad479e4f246e81d45fe6cb710af
David Szenczewski <david@Davids-Air.fios-router.home>
2019-11-16 20:13:26 -0500

    Firebase added

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -5,14 +5,14 @@ import 'firebase/auth';
 // THIS IS USED TO INITIALIZE THE firebase OBJECT
 // PUT YOUR FIREBASE PROJECT CONFIG STUFF HERE
 var firebaseConfig = {
-    apiKey: "AIzaSyCJxkqx-6PMJrZ7ACkrgbO55b5wmJdop1Y",
-    authDomain: "todo-rrf-316.firebaseapp.com",
-    databaseURL: "https://todo-rrf-316.firebaseio.com",
-    projectId: "todo-rrf-316",
-    storageBucket: "todo-rrf-316.appspot.com",
-    messagingSenderId: "892398996038",
-    appId: "1:892398996038:web:1fb9157fc6c5d266e01847",
-    measurementId: "G-TEGQB3MZ23"
+    apiKey: "AIzaSyB9pow_XLfQinvdIfqWsFbTCNDRRgf3_Co",
+    authDomain: "todo-hw3-43958.firebaseapp.com",
+    databaseURL: "https://todo-hw3-43958.firebaseio.com",
+    projectId: "todo-hw3-43958",
+    storageBucket: "todo-hw3-43958.appspot.com",
+    messagingSenderId: "1095805066824",
+    appId: "1:1095805066824:web:e8c17e6c23fe8079f4427d",
+    measurementId: "G-MJEZPEE679"
 };
 firebase.initializeApp(firebaseConfig);
 
